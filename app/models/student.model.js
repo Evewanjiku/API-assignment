866878ca2835c59801b8a0670143384d8bba8232
Evewanjiku <wanjikugitonga.eve@gmail.com>
2020-04-16 21:05:11 +0300

    Add all my files

---
+++ app/models/student.model.js
@@ -0,0 +1,98 @@
+const sql = require("./db.js");
+
+// constructor
+const Student = function(student) {
+  this.fname = student.name;
+  this.lname = student.name;
+  this.email = student.email;
+  this.active = student.active;
+};
+
+Student.create = (newStudent, result) => {
+  sql.query("INSERT INTO students SET ?", newStudent, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created student: ", { id: res.insertId, ...newStudent });
+    result(null, { id: res.insertId, ...newStudent });
+  });
+};
+
+Student.findById = (studentId, result) => {
+  sql.query(`SELECT * FROM students WHERE id = ${studentId}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found student: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // no student with that id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Student.getAll = result => {
+  sql.query("SELECT * FROM students", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("students: ", res);
+    result(null, res);
+  });
+};
+
+Student.updateById = (id, student, result) => {
+  sql.query(
+    "UPDATE students SET fname = ?, lname = ?, email=?, active = ? WHERE id = ?",
+    [student.name, student.name, student.email, student.active, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Student with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated student: ", { id: id, ...student });
+      result(null, { id: id, ...student });
+    }
+  );
+};
+
+Student.remove = (id, result) => {
+  sql.query("DELETE FROM students WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found Student with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted student with id: ", id);
+    result(null, res);
+  });
+};
+
+module.exports = Student;
